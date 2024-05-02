3154d2f5208345ec4f2da699a4de48af96020964
SylverVB <victorelish@gmail.com>
2024-05-02 02:22:53 -0400

    First commit

---
+++ Python_Loop_Statements.py
@@ -0,0 +1,44 @@
+# Lesson 4: Assignments | Python Loop Statements
+
+# 1. The Range Riddle
+
+import random
+
+moods = ["Happy", "Sad", "Energetic", "Calm"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+
+for i in range(len(days)):
+    if days[i] == "Sunday":
+        print(f'On Sunday, you were feeling {random.choice(moods)}')
+        break                                                         # After reaching Sunday and printing the message it exits the loop
+    print(f'On {days[i]}, you were feeling {random.choice(moods)}')
+print("=======================================")
+
+# 2. Double Scoop with Nested Loops
+
+import random
+
+moods = ["Happy", "Sad", "Energetic", "Calm"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+times = ["morning", "afternoon", "evening"]
+
+# Option 1. Mood and time of the day are random choices. The loop iterates over each day of the week and only one random time of the day.
+
+for i in range(len(days)):
+    for time in times:
+        if days[i] == "Sunday":
+            print(f'On Sunday {random.choice(times)}, you were feeling {random.choice(moods)}')
+            break
+        print(f'On {days[i]} {random.choice(times)}, you were feeling {random.choice(moods)}')
+        break
+print("=======================================")
+
+# Option 2. Mood is the only random choice. The loop iterates over each day of the week and each time of the day.
+
+for i in range(len(days)):
+    for time in times:
+        if days[i] == "Sunday":
+            print(f'On Sunday {time}, you were feeling {random.choice(moods)}')
+            break
+        print(f'On {days[i]} {time}, you were feeling {random.choice(moods)}')
