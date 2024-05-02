9ecc5a2a9bbd22fe52f00df18b3e640183de4f81
SylverVB <victorelish@gmail.com>
2024-05-02 10:23:46 -0400

    Added another option

--- Python_Loop_Statements.py
+++ Python_Loop_Statements.py
@@ -7,6 +7,14 @@ import random
 moods = ["Happy", "Sad", "Energetic", "Calm"]
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
+# Option 1.
+
+for i in range(len(days)):
+    print(f'On {days[i]}, you were feeling {random.choice(moods)}')
+
+print("=======================================")
+
+# Option 2.
 
 for i in range(len(days)):
     if days[i] == "Sunday":
